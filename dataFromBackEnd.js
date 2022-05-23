311b9469a59cdf38095e47b235c15a3ba0b6ab3a
Andrey <93935023+desertptu4ka@users.noreply.github.com>
2022-05-23 15:43:46 +0300

    the commit

---
+++ dataFromBackEnd.js
@@ -0,0 +1,27 @@
+const banks = [
+  {
+    id: 1,
+    name: "bank - 1",
+    interestRate: 2,
+    maxLoan: 500000,
+    minPayment: 1000,
+    loanTerm: 12,
+  },
+  {
+    id: 2,
+    name: "bank - 2",
+    interestRate: 5,
+    maxLoan: 25000,
+    minPayment: 1500,
+    loanTerm: 1,
+  },
+  {
+    id: 3,
+    name: "bank - 3",
+    interestRate: 5,
+    maxLoan: 200,
+    minPayment: 100,
+    loanTerm: 22,
+    },
+  
+];
